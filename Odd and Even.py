ddd27b56fd83ca7702fc33bba5fcb48463804d21
Anubhav Yadav <121173898+AnubhavYadavBCA25@users.noreply.github.com>
2023-03-05 12:52:34 +0530

    Add files via upload

---
+++ Odd and Even.py
@@ -0,0 +1,10 @@
+# Step 1: Take input from user.
+num = int(input("Enter your number:"))
+
+# Step 2: Set condition 'if' if num is even.
+if num%2 == 0:
+    print(num, "is even number.")
+
+#Step 3: Set condition 'else' if num is odd. 
+else:
+    print(num, "is odd number.")
