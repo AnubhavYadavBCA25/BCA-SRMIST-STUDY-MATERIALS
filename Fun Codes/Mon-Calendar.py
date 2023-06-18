598fb03161c77297aa72726688448d696db1cd2d
Anubhav Yadav <121173898+AnubhavYadavBCA25@users.noreply.github.com>
2023-06-18 11:21:54 +0530

    Add files via upload

---
+++ Fun Codes/Mon-Calendar.py
@@ -0,0 +1,8 @@
+print("Welcome To The Python Calendar :) !\n")
+
+import calendar
+
+year = int(input("Enter the Year:"))
+month = int(input("Enter the Month:"))
+
+print('\n',calendar.month(year,month))
