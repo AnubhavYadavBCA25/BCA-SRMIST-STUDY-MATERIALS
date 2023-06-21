acedb2f5030869061740b092b98c5230376491b3
Anubhav Yadav <121173898+AnubhavYadavBCA25@users.noreply.github.com>
2023-06-21 12:13:12 +0530

    Guess_The_Num.py

--- Fun Codes/Guess_The_Num.py
+++ Fun Codes/Guess_The_Num.py
@@ -4,7 +4,7 @@ import random
 actual_number = int(random.randint(1, 30))
 
 
-moves_take = 0
+moves_take = 1
 while moves_take < 6:
     try:
         guess_number = int(input("Guess A Number Between 1 to 30: "))
@@ -17,7 +17,7 @@ while moves_take < 6:
     elif guess_number > actual_number:
         print("Your Guess Is Too High.")
     else:
-        print("Congratulation you guessed the correct number after {} guesses.".format(moves_take))
+        print("Congratulation you guessed the correct number in {} guesses.".format(moves_take))
         break
 
     moves_take += 1
@@ -26,7 +26,7 @@ while moves_take < 6:
         print("\nSorry, You Lose. The Number Was",actual_number)
         play_again = input("Do You Want To Play Again? (yes/no): ")
         if play_again == 'yes':
-            moves_take = 0
+            moves_take = 1
             actual_number = int(random.randint(1, 20))
         else:
             break
